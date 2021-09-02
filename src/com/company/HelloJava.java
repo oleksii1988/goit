f7272259dc3baf62e0e956e31b7fc0cea4ef86a3
AlexLytvynenko <oilytvynenko@gmail.com>
2021-09-02 20:02:30 +0300

    Second commit

--- src/com/company/HelloJava.java
+++ src/com/company/HelloJava.java
@@ -1,9 +1,8 @@
 package com.company;
 
 public class HelloJava {
+public static void main (String [] args){
+    System.out.println("Hello Java");
+}
 
-    public static void main(String[] args) {
-        System.out.println("Hello Java");
-       
-    }
 }
