d763cfee2b9f842ba9f585ca39ab2a26fe7ba02e
AlexLytvynenko <oilytvynenko@gmail.com>
2021-09-19 21:43:49 +0300

    Home work module 3 task 12

--- src/com/company/Massive.java
+++ src/com/company/Massive.java
@@ -8,20 +8,21 @@ public class Massive {
     public static void main(String[] args) {
 
 
-        String[][] strings = new String[5][3];
-        strings[0][0] = "Hello";
-        strings[0][1] = "Java";
-        strings[0][3] = "Vasia";
-        strings[1][0] = "Petia";
-        strings[1][1] = "Hahaha";
-        strings[1][2] = "Hahaha";
+        int[][] matrics = {{1,2,3},{4,5,6},{7,8,9},{4,2},{100}};
 
-     for(int i = 0; i< strings.length; i++){
 
-         System.out.println(Arrays.toString(strings));
+        for(int i = 0; i< matrics.length;i++){
+
+
+            System.out.println(Arrays.toString(matrics[i]));
+
+        }
+
+
+
      }
 
 
     }
 
-}
+
--- src/com/company/Module3Task11.java
+++ src/com/company/Module3Task11.java
@@ -5,22 +5,39 @@ import java.util.Arrays;
 public class Module3Task11 {
 
 
+    public static char[][] createKeyboard() {
 
-    public static char[][] createKeyboard(){
-
-        char[][] chart = {{'1', '2', '3'} ,{'4', '5', '6'},{'7', '8', '9'}, {'*', '0', '#'}};
+        char[][] chart =
+                        {{'1', '2', '3'},
+                        {'4', '5', '6'},
+                        {'7', '8', '9'},
+                        {'*', '0', '#'}};
 
 
         return chart;
 
 
+    }
+
+
+    public static void printKeyboard() {
+
+
+        for (int i = 0; i < createKeyboard().length; i++) {
+
+
+
+                System.out.println(Arrays.toString(createKeyboard()[i]) +"");
+
+            }
+
+        }
+
+        public static void main (String[]args){
+
+            printKeyboard();
+        }
 
 
     }
 
-    public static void main(String[] args) {
-
-        System.out.println(Arrays.deepToString(createKeyboard()));
-    }
-
-}
